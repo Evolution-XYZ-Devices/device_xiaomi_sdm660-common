e028416e7b2ab2569de69d5c6d1c86ba96143097
Martin <recordyletsplays@gmail.com>
2020-06-04 12:51:48 +0200

    sdm660-common: inherit GCam if exists

--- sdm660.mk
+++ sdm660.mk
@@ -34,6 +34,7 @@ endif
 # Inherit proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
+$(call inherit-product-if-exists, packages/apps/GoogleCamera/gcam.mk)
 $(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
 
 # Common Tree Path
