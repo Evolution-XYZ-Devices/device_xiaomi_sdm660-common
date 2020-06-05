f531bf8c6f83f818cd8e750b64ddacd97129f35f
Martin <recordyletsplays@gmail.com>
2020-06-05 15:46:18 +0200

    sdm660-common: don't include IMS jar as it is already in Evo source

--- sdm660.mk
+++ sdm660.mk
@@ -285,10 +285,6 @@ PRODUCT_PACKAGES += \
     ims-ext-common_system \
     ims_ext_common.xml \
 
-# IMS JAR
-PRODUCT_BOOT_JARS += \
-    ims-ext-common_system
-
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
