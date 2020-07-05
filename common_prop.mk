b35650aa8612d39ae233b3747ef6e35280cf7269
Martin <recordyletsplays@gmail.com>
2020-07-05 17:23:11 +0200

    sdm660-common: enable WiFi calls in all regions

--- common_prop.mk
+++ common_prop.mk
@@ -191,6 +191,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.vendor.ims.disableUserAgent=0
 
