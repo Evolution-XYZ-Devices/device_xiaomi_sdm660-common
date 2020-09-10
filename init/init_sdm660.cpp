7ebced7060e743665bdfb807a1ea3263571e70ce
Martin <recordyletsplays@gmail.com>
2020-09-10 18:08:09 +0200

    sdm660-common: Update fingerprint to Coral RP1A.200720.009
    If the old fingerprint is left in place, SafetyNet will fail.
    However, it seems google doesn't check if the Android versions match up so we can use the Android 11 one and pass SafetyNet

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -140,5 +140,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "lavender-user 9 PKQ1.180904.001 V11.0.8.0.PFGEUXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys");
 }
