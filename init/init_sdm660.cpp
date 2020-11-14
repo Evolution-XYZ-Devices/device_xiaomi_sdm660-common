89ef6ecc4473747679b34ce016222bf97414912d
Martin <recordyletsplays@gmail.com>
2020-11-15 00:30:46 +0100

    sdm660-common: Handle fingerprint overrides through init
    Conflicts:
    	init/init_sdm660.cpp

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -118,8 +118,8 @@ void vendor_load_persist_properties()
 }
 void vendor_load_properties()
 {
-  property_override("ro.build.description", "xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys");
-  property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys");
+  property_override("ro.build.description", "lavender-user 10 QKQ1.190910.002 V11.0.1.0.QFGMIXM release-keys");
+  property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys");
   
    std::string product = GetProperty("ro.product.vendor.device", "");	
    if (product.find("whyred") != std::string::npos)
