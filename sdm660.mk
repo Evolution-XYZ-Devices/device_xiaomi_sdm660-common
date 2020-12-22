00f5d2ac2f55ed2522553b8a2b8f0ee43e2b0bee
Martin <recordyletsplays@gmail.com>
2020-12-23 00:36:13 +0100

    sdm660-common: Nuke LOS overlays completely

--- sdm660.mk
+++ sdm660.mk
@@ -384,7 +384,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
-    $(COMMON_PATH)/overlay-lineage \
     $(COMMON_PATH)/overlay-evo
 
 # RRO configuration
