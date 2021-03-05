4a6cc9e2bae2a1f6fcab8bce3cbef20c78714108
Omar Hamad <etahamad@icloud.com>
2021-03-05 17:23:12 +0200

    sdm660-common: DeviceSettings: Implement Clear Speaker
    Co-authored-by: Adithya R <radithya2002@gmail.com>
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    
    Conflicts:
    	DeviceSettings/src/org/lineageos/settings/device/DeviceSettings.java
    
    Conflicts:
    	DeviceSettings/res/xml/preferences_xiaomi_parts.xml

--- DeviceSettings/src/org/lineageos/settings/device/DeviceSettings.java
+++ DeviceSettings/src/org/lineageos/settings/device/DeviceSettings.java
@@ -24,6 +24,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 
 import org.lineageos.settings.device.kcal.KCalSettingsActivity;
+import org.lineageos.settings.device.speaker.ClearSpeakerActivity;
 import org.lineageos.settings.device.preferences.SecureSettingListPreference;
 import org.lineageos.settings.device.preferences.SecureSettingSwitchPreference;
 import org.lineageos.settings.device.preferences.VibrationSeekBarPreference;
@@ -71,6 +72,9 @@ public class DeviceSettings extends PreferenceFragment implements
 
     private static final String DEVICE_JASON_PACKAGE_NAME = "org.lineageos.settings.devicex";
     private static final String PREF_DEVICE_JASON = "device_jason";
+    
+    private static final String PREF_CLEAR_SPEAKER = "clear_speaker_settings";
+    private Preference mClearSpeakerPref;
 
     private SecureSettingSwitchPreference mEnableDirac;
     private SecureSettingListPreference mHeadsetType;
@@ -79,6 +83,14 @@ public class DeviceSettings extends PreferenceFragment implements
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences_xiaomi_parts, rootKey);
+        
+        mClearSpeakerPref = (Preference) findPreference(PREF_CLEAR_SPEAKER);
+        mClearSpeakerPref.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), ClearSpeakerActivity.class);
+            startActivity(intent);
+            return true;
+        });
+
 
         // LED Brightness
         if (FileUtils.fileWritable(NOTIF_LED_PATH)) {
