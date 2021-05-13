f40b0a7972448fc2bd8c1f7c0494ee6a65754d0e
clarencelol <clarencekuiek@icloud.com>
2021-05-13 11:51:08 +0800

    sdm660-common: Build Pixel powerstats
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -456,7 +456,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi_sdm660-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service.xiaomi_sdm660
 
 # Powerhint
 ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
