554b90fd3dc22df715256c87d2cee5967410c95a
Pranav Vashi <neobuddy89@gmail.com>
2021-06-05 15:10:03 +0800

    sdm660-common: DeviceSettings: Switch back to default thermal profile when screen off
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- DeviceSettings/src/org/lineageos/settings/device/thermal/ThermalUtils.java
+++ DeviceSettings/src/org/lineageos/settings/device/thermal/ThermalUtils.java
@@ -134,6 +134,10 @@ public final class ThermalUtils {
         return state;
     }
 
+    protected void setDefaultThermalProfile() {
+        FileUtils.setValue(THERMAL_SCONFIG, THERMAL_STATE_DEFAULT);
+    }
+
     protected void setThermalProfile(String packageName) {
         String value = getValue();
         String modes[];
