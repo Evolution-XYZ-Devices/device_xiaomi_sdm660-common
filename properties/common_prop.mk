61aee2a1a02256d54f186ef6dd1675ab18cb98e6
Himanshu Agrawal <hagraw@codeaurora.org>
2021-07-26 16:33:29 +0800

    sdm660-common: Disable ahardware buffer
    - Disable ahardware buffer usage in gralloc
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- properties/common_prop.mk
+++ properties/common_prop.mk
@@ -169,6 +169,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_buffer_age=false \
     vendor.display.disable_skip_validate=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.gralloc.disable_ahardware_buffer=1 \
     debug.gralloc.enable_fb_ubwc=1 \
     vendor.display.enable_default_color_mode=0 \
     vendor.opengles.version=196610 \
