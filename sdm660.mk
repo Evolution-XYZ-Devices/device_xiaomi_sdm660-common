5115b00bd03f3dad9d9b2efa135248d3d4b11bfb
Park Ju Hyung <qkrwngud825@gmail.com>
2021-08-22 16:34:40 +0800

    sdm660-common: Optimize everything on preopt
    We have plenty of space left at /system
    
    Change-Id: I39d552259b66ec329c56cd4f4a7fc46fb2c07457
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: orgesified <gesishabani1@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -545,7 +545,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel
 
 # Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := everything
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 # Tetheroffload
