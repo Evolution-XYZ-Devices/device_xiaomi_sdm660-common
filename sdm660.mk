d68f60e755bdc4142d2ff8420e3d0ad9b596d0b9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-14 16:24:25 +0800

    sdm660-common: libhidl: Move to Android.bp
    Change-Id: I3945a460d563aaf07aaa7b757bb9c0dddc4f91cd
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -283,7 +283,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_vendor \
+    android.hidl.base@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
