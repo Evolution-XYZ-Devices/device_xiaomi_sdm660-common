a12a79658d1ed64dbe1fa71ec63aee933d15c39a
Michael Bestas <mkbestas@lineageos.org>
2021-10-04 23:26:56 +0200

    sdm660-common: props: Remove coresight leftovers
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- properties/common_prop.mk
+++ properties/common_prop.mk
@@ -233,7 +233,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.df.dev_name=rmnet_usb0 \
-    persist.debug.coresight.config=stm-events \
     persist.vendor.dpmhalservice.enable=1 \
     persist.vendor.qti.telephony.vt_cam_interface=2 \
     persist.vendor.radio.add_power_save=1 \
