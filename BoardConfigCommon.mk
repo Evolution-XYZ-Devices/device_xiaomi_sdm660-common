783bab1060a25c769b4917e33a392d6239dc17b6
Omar Hamad <etahamad@icloud.com>
2021-10-06 11:25:37 +0200

    sdm660-common: Adapt sepolicy flags to Android 12
    private/public
    
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -220,8 +220,8 @@ PROTOBUF_SUPPORTED := true
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Timeservice
 BOARD_USES_QC_TIME_SERVICES := true
