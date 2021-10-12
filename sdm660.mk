bd8c8e0bd4195931ce1049547ef652f20d26f416
Omar Hamad <etahamad@icloud.com>
2021-10-12 19:25:43 +0200

    sdm660-common: Build more and more and more HIDL targets
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -303,9 +303,13 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.drm@1.4.vendor \
+    android.hardware.gatekeeper@1.0 \
     android.hardware.gatekeeper@1.0.vendor \
     android.hardware.media.c2@1.1.vendor \
+    android.hardware.neuralnetworks@1.3 \
     android.hardware.neuralnetworks@1.3.vendor \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service \
     android.hardware.keymaster@4.0.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio@1.4.vendor \
