61a5ba7ce73f1fb67eb7fba1b5f3a077974ee0cf
Martin Štrobl <recordyletsplays@gmail.com>
2021-10-15 21:05:48 +0200

    sdm660-common: Build android.hardware.secure_element@1.2
    * Might as well remove it from the manifest, since it's not required
    * Fixes error:
     F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.secure_element@1.2.so" not found: needed by /vendor/lib64/libril-qc-hal-qmi.so in namespace (default)
    
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -309,7 +309,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor \
     android.system.net.netd@1.1.vendor \
     android.hardware.gnss@2.1.vendor
 
@@ -515,7 +515,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.1 \
-    android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.2 \
     librmnetctl \
     libxml2 \
     libprotobuf-cpp-full \
