f4b27705c9b314073a8704eb9045e5a5e423d9c4
Martin Štrobl <recordyletsplays@gmail.com>
2021-10-18 20:41:24 +0200

    sdm660-common: Go Enforcing!
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -160,7 +160,6 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_sdm660
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
