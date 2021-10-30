f79edfa82d5e73c0e25acb19fba76f5419ed59f7
pix106 <sbordenave@gmail.com>
2021-10-30 09:10:44 +0200

    sdm660-common: build android.hardware.drm@1.4.vendor
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -231,7 +231,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
