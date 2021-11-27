0f6dd3cf4e2281532d4643501202ca1c89538d6f
pix106 <sbordenave@gmail.com>
2021-11-27 16:30:03 +0100

    sdm660-common: build android.hardware.radio.config 1.1
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -290,6 +290,7 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor \
     android.hardware.keymaster@3.0.vendor \
     android.hardware.keymaster@4.0.vendor \
+    android.hardware.radio.config@1.1.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
