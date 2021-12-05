b7441c63f55425c11217b51142af12ae487324a2
TheScarastic <warabhishek@gmail.com>
2021-12-05 08:23:40 +0100

    sdm660-common: DeviceSettings: Adapt to S style
    Co-authored-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- DeviceSettings/src/org/lineageos/settings/device/DeviceSettingsActivity.java
+++ DeviceSettings/src/org/lineageos/settings/device/DeviceSettingsActivity.java
@@ -16,35 +16,28 @@
 
 package org.lineageos.settings.device;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class DeviceSettingsActivity extends Activity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class DeviceSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private DeviceSettings deviceSettingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
-        DeviceSettings deviceSettingsFragment;
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.content_frame);
         if (fragment == null) {
             deviceSettingsFragment = new DeviceSettings();
             getFragmentManager().beginTransaction()
-                    .add(android.R.id.content, deviceSettingsFragment)
+                    .add(R.id.content_frame, deviceSettingsFragment)
                     .commit();
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
