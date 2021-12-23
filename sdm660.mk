ffd32112d55200630101ad8a33f8337991ed9a0b
pix106 <sbordenave@gmail.com>
2021-12-23 13:00:59 +0100

    Revert "sdm660-common: Ship copy of API 28 cgroups/task_profiles.json in vendor"
    This reverts commit 28cff7a1a777fef6b84c7ea32c2c1a58b5fabfcb.
    
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Martin Štrobl <recordyletsplays@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -162,11 +162,6 @@ PRODUCT_PACKAGES += \
     com.android.media.swcodec \
     libsfplugin_ccodec
 
-# Cgroup and task_profiles
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
