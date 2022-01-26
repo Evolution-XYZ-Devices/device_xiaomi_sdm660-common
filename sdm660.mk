7e275231c45b85107a15826800c1cd887baf264b
baalajimaestro <me@baalajimaestro.me>
2022-01-26 15:54:35 +0600

    sdm660-common: prop: Optimise dex flags
    This is a squash of the following commits:
    1. Optimise dex flags for a faster boot
    
    * Used multiple threads and speed profile to hasten the first boot
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: I2cce5ddf7d50308511e81436fcac613b2c6537bf
    
    2. Rework dex flags again
    
    When I went through https://source.android.com/devices/tech/dalvik/configure my previous configs felt wrong, this one should be perfect (I hope).
    Even though there is a slight trade-off for boot time by using the speed profile, we do make up for it by using 8 threads.
    PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER attempts to compile all prebuilts fully optimised to speed level, thus the phone doesnt need to deal with it.
    I also do know that this might be a trade-off on size, but I do see the first boot time is more worthy to trade-off.
    The speed level mentioned here runs dex verification and compiles all AOT methods.
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: Ic0156ff8956a7c5f53820be6db438f6f6399d3c0
    
    3. Switch certain dexopt profiles to verify
    
    Prebuilt apps like Gmail/Google App, will be updated by google play, and there is no need spend time and space optimising what is going to be replaced.
    Switch to Google's recommendation of using verify for an OTA package.
    Applications still stay on speed.
    Instead, replace the install profile with speed-profile making apps perform better.
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: I2cce5ddf7d50308511e81436fcac613b2c6537bf
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -149,6 +149,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DeviceDoze
 
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
