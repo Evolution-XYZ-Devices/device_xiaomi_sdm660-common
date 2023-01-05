c645f00c817c6ad44ba961051c0f0ee1a077dd37
FlowerSea0208 <zzp060208@gmail.com>
2023-01-05 14:41:12 +0800

    sdm660-common: rootdir: Add init.xiaomi.rc

--- sdm660.mk
+++ sdm660.mk
@@ -276,6 +276,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.recovery.qcom.rc \
     init.target.rc \
+    init.xiaomi.rc \
     init.xiaomi_parts.rc \
     ueventd.qcom.rc \
     apex_metadata.rc
