50da503549de63f0966d3a85ccfc3fe211666f0b
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    sdm660-common: Build lineage health HAL
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: Le Hong Duc <hongducthbk123@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -302,6 +302,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm660
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
