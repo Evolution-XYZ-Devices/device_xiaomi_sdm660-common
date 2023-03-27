45d51caaff2cc38cd3b7a42296bff31020827135
Alcatraz323 <alcatraz32323@gmail.com>
2023-03-27 22:34:59 +0800

    sdm660-common: use QTI USB 1.3 HAL
    * follows vendor/qcom/opensource/usb/vendor_product.mk
    * enable full usb function
    * partially ported sepolicy_vndr
    
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -418,15 +418,14 @@ PRODUCT_BOOT_JARS += \
 
 # USB
 PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service-qti
+
+PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
