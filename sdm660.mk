29a6856ef0c286b21028cec8075f922388323ea2
krasCGQ <kras@raphielgang.org>
2023-06-14 18:33:55 +0300

    sdm660-common: Inherit several Android Go configurations
    * Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Android GO
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
