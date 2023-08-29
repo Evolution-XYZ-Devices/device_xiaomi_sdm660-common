e64cd5a7221cf3b0e8339424caace7ba578b3329
Omar Mohamed <omarcoptan9@gmail.com>
2023-08-29 08:32:23 +0000

    Revert "sdm660-common: Move to common Xiaomi lights AIDL"
    This reverts commit 6d6b714ad355d581267deb383a3ff44c27177f03.

--- sdm660.mk
+++ sdm660.mk
@@ -282,7 +282,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light@2.0-service.xiaomi_sdm660
 
 # Media
 PRODUCT_COPY_FILES += \
