0367e96638bde8ab827a5022a1d1c54cd73159ef
Omar Mohamed <omarcoptan9@gmail.com>
2023-09-16 22:07:04 +0000

    [TEMP]sdm660-common: Don't build init.msm.usb.configfs.rc from USB HAL repo

--- sdm660.mk
+++ sdm660.mk
@@ -252,6 +252,7 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
+    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh
