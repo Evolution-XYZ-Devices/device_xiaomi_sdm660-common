83f6bd23c7dafd0debd090b73a2063f881776609
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-10 22:04:18 -0700

    sdm660-common: Build `android.frameworks.sensorservice@1.0.vendor`
    Change-Id: I37248601556717f32e82940a2adfdbea4f4db91b
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -399,7 +399,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0.vendor \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
