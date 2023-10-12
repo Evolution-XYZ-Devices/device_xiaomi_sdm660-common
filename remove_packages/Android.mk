bf7d5091ff93e7f0b2fd231b999bf673a50a3f36
Le Hong Duc <hongducthbk123@gmail.com>
2023-10-12 23:00:40 +0700

    sdm660-common: remove_packages: Restore some packages We now have retrofitted partition, let's restore some Google bloatware

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,13 +4,28 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup ConnMO DCMO
-LOCAL_OVERRIDES_PACKAGES += DMService
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Showcase
-LOCAL_OVERRIDES_PACKAGES += SprintDM SprintHM USCCDM
-LOCAL_OVERRIDES_PACKAGES += VZWAPNLib VzwOmaTrigger obdm_stub
+LOCAL_OVERRIDES_PACKAGES := \
+    AppDirectedSMSService \
+    arcore \
+    BetterBugStub \
+    ConnMO \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DMService \
+    Drive \
+    MaestroPrebuilt \
+    Maps \
+    PlayAutoInstallConfig \
+    PrebuiltGmail \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    SoundAmplifierPrebuilt \
+    talkback \
+    Tycho \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    WfcActivation
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
