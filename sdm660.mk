1ffde9eadbfa687748036a7eda4bfbaa0a7a15cf
Omar Mohamed <omarcoptan9@gmail.com>
2024-02-03 16:56:51 +0000

    sdm660-common: Switch to GCamGOPrebuilt-V1
    * More lightweight

--- sdm660.mk
+++ sdm660.mk
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    GCamGOPrebuilt-V3_8 \
+    GCamGOPrebuilt \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service
 
