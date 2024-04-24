2a4c05be4f204b838ac21035764a112e4e3e70f2
Omar Mohamed <omarcoptan9@gmail.com>
2024-04-24 23:18:26 +0000

    sdm660-common: Switch to Aperture

--- sdm660.mk
+++ sdm660.mk
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    GCamGOPrebuilt-V3 \
+    Aperture \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service
 
