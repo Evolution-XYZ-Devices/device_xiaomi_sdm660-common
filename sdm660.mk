fc75ca136c0011b02d56e1c8830f0c4bbacdf9f1
tweenk <tweenk.pl@gmail.com>
2024-04-24 13:31:05 +0530

    sdm660-common: Build prebuilt protobuf 3.9.1.
    Needed to work around a Qualcomm HAL prebuilt.
    
    *Fixes:-
      04-24 13:21:51.858  9186  9186 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrilNrd": library "libprotobuf-cpp-full-3.9.1.so" not found: needed by /vendor/lib64/libqcrilNr.so in namespace (default)
      04-24 13:21:51.861  9187  9187 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrilNrd": library "libprotobuf-cpp-full-3.9.1.so" not found: needed by /vendor/lib64/libqcrilNr.so in namespace (default)
    
    Bug: 203713560
    Bug: 258322017
    Bug: 258589373
    Bug: 258632688
    Test: Forrest
    Change-Id: I7c45f92295161537944175dc006ef4fc03d805ba
    Signed-off-by: kartik-commits <kartiklashkare@gmail.com>
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -400,8 +400,8 @@ PRODUCT_COPY_FILES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QMI
 PRODUCT_PACKAGES += \
