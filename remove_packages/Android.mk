215f98fabf9a39e4372e5df8eca8c8d2d7cf0589
Omar Mohamed <omarcoptan9@gmail.com>
2024-04-25 06:44:06 +0000

    Revert "sdm660-common: RemovePackages: Move to Android.bp"
    This reverts commit 24b54d6f6d680d368126019124bbb97832c7b756.

---
+++ remove_packages/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    AppDirectedSMSService \
+    BetterBugStub \
+    ConnMO \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DMService \
+    Drive \
+    MaestroPrebuilt \
+    Maps \
+    PlayAutoInstallConfig \
+    PrebuiltGmail \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    SoundAmplifierPrebuilt \
+    talkback \
+    Tycho \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    WfcActivation
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
